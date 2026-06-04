07c4429bc66d982d8ac9d809a74b11294aeee917
ZiyanRana <mziyanrana@gmail.com>
2026-06-04 10:05:13 +0500

    Update environment variables

--- Backend/src/config/env.js
+++ Backend/src/config/env.js
@@ -6,5 +6,9 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 export const {
     PORT,
     NODE_ENV,
-    MONGODB_URI
+    MONGODB_URI,
+    JWT_SECRET,
+    REFRESH_TOKEN_EXPIRES_IN,
+    ACCESS_TOKEN_EXPIRES_IN,
+    COOKIE_EXPIRES_IN_DAYS
 } = process.env;
