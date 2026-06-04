d47b3805077fc3942a95b6120be149f0b9e1d7ff
ZiyanRana <mziyanrana@gmail.com>
2026-06-05 00:01:32 +0500

    Add OTP model

---
+++ Backend/src/models/otp.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const otpSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'User reference is required!']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required!']
+    },
+    otp: {
+        type: String,
+        required: [true, 'OTP is required!'],
+        match: [/^\d{6}$/, 'OTP must be exactly 6 digits!']
+    },
+    expiresAt: {
+        type: Date,
+        required: [true, 'Expiration time is required!'],
+        expires: 0
+    }
+}, { timestamps: true });
+
+const otpModel = mongoose.model('OTP', otpSchema);
+
+export default otpModel;
