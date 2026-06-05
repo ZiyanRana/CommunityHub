6d87cb9c350b26634438d7defc38cea02ed1cd4d
ZiyanRana <mziyanrana@gmail.com>
2026-06-05 16:14:17 +0500

    Refactor post routes

--- Backend/src/routes/post.routes.js
+++ Backend/src/routes/post.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { getPosts, createPost, getPost, updatePost, deletePost } from '../controllers/post.controller.js';
+import { getPosts, createPost, getPost, editPost, deletePost } from '../controllers/post.controller.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const postRouter = express.Router();
 
 // Path: /api/v1/posts
-postRouter.get('/', getPosts);
-postRouter.post('/', createPost);
-postRouter.get('/:id', getPost);
-postRouter.put('/:id', updatePost);
-postRouter.delete('/:id', deletePost);
+postRouter.get('/', authMiddleware, getPosts);
+postRouter.post('/', authMiddleware, createPost);
+postRouter.get('/:id', authMiddleware, getPost);
+postRouter.put('/:id', authMiddleware, editPost);
+postRouter.delete('/:id', authMiddleware, deletePost);
 
 export default postRouter;
