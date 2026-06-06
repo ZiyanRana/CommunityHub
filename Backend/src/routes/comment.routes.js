ee2f2f6a39416ebc2180b8fcfdd37c643ba87178
ZiyanRana <mziyanrana@gmail.com>
2026-06-06 15:49:26 +0500

    Update project files

--- Backend/src/routes/comment.routes.js
+++ Backend/src/routes/comment.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { getComments, commentOnPost, deleteComment, editComment } from '../controllers/comment.controller.js';
+import { getPostComments, commentOnPost, deleteComment, editComment } from '../controllers/comment.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const commentRouter = express.Router();
 
 // Path: /api/v1/comments
-commentRouter.get('/:id', authMiddleware, getComments);
+commentRouter.get('/:id', authMiddleware, getPostComments);
 commentRouter.post('/:id', authMiddleware, commentOnPost);
 commentRouter.patch('/:id/:commentId', authMiddleware, editComment);
 commentRouter.delete('/:id/:commentId', authMiddleware, deleteComment);
