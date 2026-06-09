2322fa425b841f8c48a2a1ed7e4ec7a1bb1b6306
ZiyanRana <mziyanrana@gmail.com>
2026-06-09 20:12:28 +0500

    Update post routes

--- Backend/src/routes/post.routes.js
+++ Backend/src/routes/post.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getPosts, createPost, searchPosts, getPost, editPost, deletePost } from '../controllers/post.controller.js';
+import { getPosts, createPost, searchPostsGeneric, searchPostsTags, searchPostsLocation, getPost, editPost, deletePost } from '../controllers/post.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const postRouter = express.Router();
@@ -7,7 +7,9 @@ const postRouter = express.Router();
 // Path: /api/v1/posts
 postRouter.get('/', authMiddleware, getPosts);
 postRouter.post('/', authMiddleware, createPost);
-postRouter.get('/search', authMiddleware, searchPosts);
+postRouter.get('/search', authMiddleware, searchPostsGeneric);
+postRouter.get('/search/tags', authMiddleware, searchPostsTags);
+postRouter.get('/search/location', authMiddleware, searchPostsLocation);
 postRouter.get('/:id', authMiddleware, getPost);
 postRouter.patch('/:id', authMiddleware, editPost);
 postRouter.delete('/:id', authMiddleware, deletePost);
