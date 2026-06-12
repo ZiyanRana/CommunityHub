80e776be8ae413b06a47f89ca828e64f17d48715
ZiyanRana <mziyanrana@gmail.com>
2026-06-12 19:10:28 +0500

    Update project files

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import postRouter from './routes/post.routes.js';
 import authRouter from './routes/auth.routes.js';
+import postRouter from './routes/post.routes.js';
+import otpRouter from './routes/otp.routes.js';
+import searchRouter from './routes/search.routes.js';
+import likeRouter from './routes/like.routes.js';
+import commentRouter from './routes/comment.routes.js';
 
 const app = express();
 
@@ -13,7 +17,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // Routes
-app.use('/api/v1/posts', postRouter);
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/posts', postRouter);
+app.use('/api/v1/otp', otpRouter);
+app.use('/api/v1/search', searchRouter);
+app.use('/api/v1/likes', likeRouter);
+app.use('/api/v1/comments', commentRouter);
 
 export default app;
