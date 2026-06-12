b31ceb10d61386ab7866e574e4b017682aa236ff
ZiyanRana <mziyanrana@gmail.com>
2026-06-12 19:59:09 +0500

    Add get user-feed route

--- Backend/src/routes/post.routes.js
+++ Backend/src/routes/post.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getPosts, createPost, getSpecificUserPosts, getPost, editPost, deletePost } from '../controllers/post.controller.js';
+import { getPosts, createPost, getSpecificUserPosts, getPost, editPost, getFeed, deletePost } from '../controllers/post.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const postRouter = express.Router();
@@ -10,6 +10,7 @@ postRouter.post('/', authMiddleware, createPost);
 postRouter.get('/:id', authMiddleware, getPost);
 postRouter.get('/user/:id', authMiddleware, getSpecificUserPosts);
 postRouter.patch('/:id', authMiddleware, editPost);
+postRouter.get('/feed', authMiddleware, getFeed);
 postRouter.delete('/:id', authMiddleware, deletePost);
 
 export default postRouter;
