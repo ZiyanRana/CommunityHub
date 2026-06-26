6b11783f8444c4a606c67165e511fcfa9ce1f8b0
ZiyanRana <mziyanrana@gmail.com>
2026-06-26 22:10:34 +0500

    Add account routes

--- Backend/src/routes/account.routes.js
+++ Backend/src/routes/account.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { editAccount, deleteAccount, toggleAccountPrivacy } from "../controllers/account.controller.js";
+import { getAccount, updateAccount, deactivateAccount, deleteAccount, toggleAccountPrivacy } from "../controllers/account.controller.js";
 
 const accountRouter = express.Router();
 
 // Path: /api/v1/account
-accountRouter.post('/edit', authMiddleware, editAccount);
+accountRouter.get('/:id', authMiddleware, getAccount);
+accountRouter.post('/update', authMiddleware, updateAccount);
+accountRouter.post('/deactivate', authMiddleware, deactivateAccount);
 accountRouter.delete('/delete', authMiddleware, deleteAccount);
 accountRouter.patch('/privacy', authMiddleware, toggleAccountPrivacy);
 
